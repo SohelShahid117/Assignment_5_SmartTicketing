7aa9765a2a6433968969d43ffd2a7e0b94266aa8
SohelShahid117 <sohelshahid09@gmail.com>
2024-02-22 16:42:22 +0600

    working in js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+function convertedValue(id){
+    const textValue = document.getElementById(id).innerText;
+    // const textValue2 = textValue.innerText;
+    const textNumber = parseInt(textValue);
+    console.log(textNumber);
+    // return textNumber; 
+}
+
+const seatLfet = convertedValue("seat-left");
+// console.log(seatLfet);
+
+// const selectSeat  = convertedValue('select-seat');
+// console.log(selectSeat);
+
+// const totalPrice = convertedValue('total-price');
+// console.log(totalPrice);
+
+// const grandTotalPrice = convertedValue('grand-total-price');
+// console.log(grandTotalPrice);
